1a01e4796200312d1fd5fa0584160056b1a6e8cf
Asif <msasifiiuc@gmail.com>
2023-12-29 23:51:21 +0600

    text editor change set up

--- src/Pages/CreateBlog/CreateBlog.jsx
+++ src/Pages/CreateBlog/CreateBlog.jsx
@@ -2,46 +2,27 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "../../components/Navbar/Navbar";
-import { useContext } from "react";
+import { useContext,  useRef, useState } from "react";
 import { AuthContext } from "../../components/Provider/AuthProvider";
-import 'quill/dist/quill.snow.css'
-import ReactQuill from 'react-quill'
+import JoditEditor from 'jodit-react';
 
 const CreateBlog = () => {
+    const editor = useRef(null);
+    const [content, setContent] = useState('');
+
+    // const config = useMemo(
+    //     {
+    //         readonly: false,
+    //         placeholder: 'Start typings...'
+    //     },
+    //     [' ']
+    // );
     const location = useLocation();
     const navigate = useNavigate();
     const { user } = useContext(AuthContext);
     //console.log(user);
     const currentEmail = user.email;
     const userImg = user?.photoURL ? user.photoURL : "https://i.ibb.co/R3PnR7z/user.png";
-    var modules = {
-        toolbar: [
-            [{ size: ["small", false, "large", "huge"] }],
-            ["bold", "italic", "underline", "strike", "blockquote"],
-            [{ list: "ordered" }, { list: "bullet" }],
-            ["link", "image"],
-            [
-                { list: "ordered" },
-                { list: "bullet" },
-                { indent: "-1" },
-                { indent: "+1" },
-                { align: [] }
-            ],
-            [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
-        ]
-    };
-
-    var formats = [
-        "header", "height", "bold", "italic",
-        "underline", "strike", "blockquote",
-        "list", "color", "bullet", "indent",
-        "link", "image", "align", "size",
-    ];
-
-    const handleProcedureContentChange = (content) => {
-        console.log("content---->", content);
-    };
-
     const handleNewBlog = (e) => {
         e.preventDefault();
         const form = e.target;
@@ -143,18 +124,17 @@ const CreateBlog = () => {
                                     <label className="label">
                                         <span className="label-text font-bold">Write Details</span>
                                     </label>
-
                                     <div>
-                                        <ReactQuill
-                                            theme="snow"
-                                            modules={modules}
-                                            formats={formats}
-                                            placeholder="write your Blog ...."
-                                            onChange={handleProcedureContentChange}
-                                            style={{ height: "220px" }}
-                                        >
-                                        </ReactQuill>
+                                        <JoditEditor
+                                            ref={editor}
+                                            value={content}
+                                            // config={config}
+                                            tabIndex={1} // tabIndex of textarea
+                                            onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+                                            onChange={newContent => setContent(newContent)}
+                                        />
                                     </div>
+
                                 </div>
                             </div>
                             <input type="submit" value="Post" className="w-full mt-6 bg-orange-600 text-white border-black border-2 text-center p-2 font-rancho text-2xl" />
--- src/Pages/UpdateBlog/UpdateBlog.jsx
+++ src/Pages/UpdateBlog/UpdateBlog.jsx
@@ -4,34 +4,8 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import { AuthContext } from "../../components/Provider/AuthProvider";
 import Navbar from "../../components/Navbar/Navbar";
-import 'quill/dist/quill.snow.css';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.bubble.css';
 
 const UpdateBlog = () => {
-    var modules = {
-        toolbar: [
-            [{ size: ["small", false, "large", "huge"] }],
-            ["bold", "italic", "underline", "strike", "blockquote"],
-            [{ list: "ordered" }, { list: "bullet" }],
-            ["link", "image"],
-            [
-                { list: "ordered" },
-                { list: "bullet" },
-                { indent: "-1" },
-                { indent: "+1" },
-                { align: [] }
-            ],
-            [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
-        ]
-    };
-
-    var formats = [
-        "header", "height", "bold", "italic",
-        "underline", "strike", "blockquote",
-        "list", "color", "bullet", "indent",
-        "link", "image", "align", "size",
-    ];
 
     const blog = useLoaderData();
     const { title, image, category, shortDescription, details, _id } = blog;
@@ -39,11 +13,7 @@ const UpdateBlog = () => {
     const navigate = useNavigate();
     const { user } = useContext(AuthContext);
     const userImg = user?.photoURL ? user.photoURL : "https://i.ibb.co/R3PnR7z/user.png";
-    const [quillContent, setQuillContent] = useState(details);
-    const handleProcedureContentChange = (content) => {
-        setQuillContent(content);
-    };
-
+    
     const handleUpdateBlog = async (e) => {
         e.preventDefault();
         const form = e.target;
@@ -68,7 +38,7 @@ const UpdateBlog = () => {
             image,
             authorImg: userImg,
             shortDescription,
-            details: quillContent,
+            details: details,
             currentDate,
             date: formattedDate
         };
@@ -156,14 +126,9 @@ const UpdateBlog = () => {
                                     <label className="label">
                                         <span className="label-text font-bold">Update Details</span>
                                     </label>
-                                    <ReactQuill
-                                        theme="snow"
-                                        modules={modules}
-                                        formats={formats}
-                                        value={quillContent}
-                                        onChange={handleProcedureContentChange}
-                                        style={{ height: "220px" }}
-                                    />
+                                    <label className="">
+                                        <textarea required name="details" defaultValue={details} placeholder="Enter Blog Details" className="textarea h-24 textarea-bordered w-full"></textarea>
+                                    </label>
                                 </div>
 
                             </div>
